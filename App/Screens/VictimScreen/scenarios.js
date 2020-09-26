bb72f6772d85b3d9758a5a4991bcc403c2eaf641
Sofia Canclini <canclini@kth.se>
2020-09-26 22:18:47 +0200

    strategy navigation initail commit

---
+++ App/Screens/VictimScreen/scenarios.js
@@ -0,0 +1,47 @@
+export const scenarios = [
+  {
+    id: 1,
+    value:
+      "Två eller flera personer pratade om mig som om att jag inte var där.",
+    strategy: "TakePlace",
+  },
+  {
+    id: 2,
+    value:
+      "Någon diskuterade frågor som rör mig, i min närvaro, utan att involvera mig.",
+    strategy: "Strategy2",
+  },
+  {
+    id: 3,
+    value:
+      "Någon skämtade om mig på ett sätt som gjorde att jag kände mig obekväm.",
+    strategy: "Strategy3",
+  },
+  {
+    id: 4,
+    value: "Någon förminskade mina åstdakommanden.",
+    strategy: "Strategy4",
+  },
+  {
+    id: 5,
+    value: "Någon undanhöll information from mig.",
+    strategy: "Strategy5",
+  },
+  {
+    id: 6,
+    value:
+      "Det hölls ett möte (formellt eller inte formellt) där jag borde varit med men som jag inte var bjuden till.",
+    strategy: "Strategy6",
+  },
+  {
+    id: 7,
+    value: "Någon fick mig att känna att vad jag än gör gör jag fel.",
+    strategy: "Strategy7",
+  },
+  {
+    id: 8,
+    value:
+      "Jag blev direkt eller indirekt kritiserad för en privat eller arbetsrelaterad situation jag inte haft kontroll över.",
+    strategy: "Strategy8",
+  },
+];
